62ea3d99c78345f62d3171f4fbdb1719e8239a16
FRENK SCOFIELD <kam_alibeyli@mail.ru>
2021-04-08 02:30:14 +0400

    04/08/2021 - 2:26 -Everything is completely over in the admin panel, only the login part remains. and link to home page

---
+++ Leon/Areas/WebCms/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leon.Areas.WebCms.Controllers
+{
+    [Area("WebCms")]
+    //[Route("WebCms/")]
+    [Route("WebCms/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Leon/Areas/WebCms/Controllers/ProductController.cs
+++ Leon/Areas/WebCms/Controllers/ProductController.cs
@@ -21,13 +21,13 @@ namespace Leon.Areas.WebCms.Controllers
     {
         private readonly MyContext _context;
         public readonly IWebHostEnvironment _env;
-       
+
 
         public ProductController(MyContext context, IWebHostEnvironment env)
         {
             _context = context;
             _env = env;
-            
+
         }
         public async Task<IActionResult> Index()
         {
@@ -36,7 +36,7 @@ namespace Leon.Areas.WebCms.Controllers
             {
                 Products = await _context.Products.Include(d => d.ProductCategory).ToListAsync(),
 
-            //    Files = await _context.Files.ToListAsync()
+                //    Files = await _context.Files.ToListAsync()
             };
 
             return View(viewModel);
@@ -109,82 +109,56 @@ namespace Leon.Areas.WebCms.Controllers
         }
 
 
-        //public async Task<IActionResult> Edit(int? Id)
-        //{
-        //    if (Id == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    ViewModel viewModel = new ViewModel();
 
-        //    viewModel.Clinics = await _context.Clinics.ToListAsync();
+        //Post part
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(IFormFile file,ViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var DataProdatc = await _context.Products.FindAsync(viewModel.Product.Id);
 
-        //    viewModel.Departments = await _context.Departments.Include(t => t.DepartmentTranslates).ToListAsync();
+                if (file != null)
+                {
+                    if (ImagesHelpers.ImageIsValid(file))
+                    {
+                        if (DataProdatc.ImageCode != null && DataProdatc.ImageCode != string.Empty)
+                        {
+                            ImagesHelpers.DeleteImage(DataProdatc.ImageCode, "img/imgProduct/");
+                        }
+                        DataProdatc.ImageCode = await ImagesHelpers.ImageUploadAsync(_env.WebRootPath, file, "img", "category");
+                    }
+                }
 
-        //    viewModel.Files = await _context.Files.ToListAsync();
+                await _context.SaveChangesAsync();
 
-        //    viewModel.Clinic_Select = new SelectList(await _context.Clinics.ToListAsync(), "Id", "Name");
+                //_context.Update(DataProdatc.ImageCode);
 
-        //    viewModel.Department_Select = new SelectList(await _context.Departments.Include(t => t.DepartmentTranslates).ToListAsync(), "Id", "Name");
 
-        //    viewModel.ClinicDoctor = await _context.ClinicDoctors.Include(t => t.ClinicDoctorTranslates).Include(c => c.Clinic)
-        //                                                         .Include(d => d.Department)
-        //                                                         .FirstOrDefaultAsync(a => a.Id == Id);
+                DataProdatc.Title = viewModel.Product.Title;
+                    DataProdatc.Description = viewModel.Product.Description;
+                    DataProdatc.Company = viewModel.Product.Company;
+                    DataProdatc.Lifting = viewModel.Product.Lifting;
+                    DataProdatc.Height = viewModel.Product.Height;
+                DataProdatc.Length = viewModel.Product.Length;
+                    DataProdatc.OperatingMode = viewModel.Product.OperatingMode;
+                    DataProdatc.DateOfProduction = viewModel.Product.DateOfProduction;
+                    DataProdatc.ProductCategoryId = viewModel.ProductCategory.Id;
+                   
 
-        //    viewModel.ClinicDoctorTranslates = viewModel.ClinicDoctor.ClinicDoctorTranslates;
 
-        //    return View(viewModel);
-        //}
+                await _context.SaveChangesAsync();
 
-        ////Post part
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(IFormFile file, ClinicDoctor clinicDoctor, List<ClinicDoctorTranslate> translates)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            if (file != null)
-        //            {
-        //                if (ImagesHelpers.ImageIsValid(file))
-        //                {
-        //                    if (clinicDoctor.ImageCode != null && clinicDoctor.ImageCode != string.Empty)
-        //                    {
-        //                        ImagesHelpers.DeleteImage(clinicDoctor.ImageCode, "Images/clinicDoctor/");
-        //                    }
-        //                    clinicDoctor.ImageCode = await _upload.Edit(file, "Images/clinicDoctor/", null, clinicDoctor.ImageCode);
-        //                }
-        //            }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
 
-        //            _context.Update(clinicDoctor);
-        //            foreach (var item in translates)
-        //            {
-        //                _context.ClinicDoctorTranslates.Update(item);
-        //                await _context.SaveChangesAsync();
-        //            }
-
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!ClinicExists(clinicDoctor.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(clinicDoctor);
-        //}
-
-        //private bool ClinicExists(int id)
-        //{
-        //    return _context.ClinicDoctors.Any(e => e.Id == id);
-        //}
+        private bool ClinicExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
         //Edit Function End
 
         // Delete Function Start
